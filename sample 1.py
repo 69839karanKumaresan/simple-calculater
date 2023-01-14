7622f23858938d3f6e70e61954101218068f8565
69839karanKumaresan <122674502+69839karanKumaresan@users.noreply.github.com>
2023-01-14 15:15:31 +0400

    Add files via upload

---
+++ sample 1.py
@@ -0,0 +1,15 @@
+print('welcome to the world of python')
+a= 11
+b= 12
+
+add=a+b
+sub=a-b
+mul=a*b
+module=a%b
+division=a/b
+
+print("addition", add)
+print("subtraction", sub)
+print("multiplication", mul)
+print("module", module)
+print("division", division)
